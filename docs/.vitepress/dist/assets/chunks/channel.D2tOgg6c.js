90808212d008556f15cc88c9843be096662be774
jasonsosen <jasonsosen@gmail.com>
2026-04-09 22:33:56 +0900

    Revise week 01 exercises to include unit conversions in calculations

--- docs/.vitepress/dist/assets/chunks/channel.D2tOgg6c.js
+++
@@ -1,1 +0,0 @@
-import{aK as o,aL as n}from"../app.D0BZcR3k.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ docs/.vitepress/dist/assets/chunks/channel.ralJP1Yq.js
@@ -0,0 +1,1 @@
+import{aK as o,aL as n}from"../app.BHi44z_Q.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
